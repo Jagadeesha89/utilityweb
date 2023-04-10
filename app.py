c0fe683e402ea6259dff595d5333847567dc2a2b
Jagadeesha Gowda <jaga.m.gowda@gmail.com>
2023-04-10 20:00:01 +0530

    Final Web App

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import streamlit as st
 def main():
     
     st.title("Welcome to Loan repayment schedule Genration")
-    Loan=int(st.number_input("Loan amount"))
-    Interest=float(st.number_input("Rate of Interest"))
-    Tenor=int(st.number_input("Number of months"))
+    Loan=int(st.text_input("Loan amount"))
+    Interest=float(st.text_input("Rate of Interest"))
+    Tenor=int(st.text_input("Number of months"))
     result=""
     genrate=st.button("Genrate Repayment Schedule")
     if genrate:
@@ -19,20 +19,27 @@ def main():
             interest=monthly_rate*balance
             principal_paid=monthly_payment - interest
             balance -= principal_paid
-            repayment_schedule.append((i+1,balance,principal_paid,interest,monthly_payment))
+            repayment_schedule.append((i+1,monthly_payment,principal_paid,interest,balance))
         print("Payment\tBalance\tPrincipal\tInterest\tPayment")
-        for i,(month,balance,principal_paid,interest,monthly_payment)in enumerate (repayment_schedule):
-            print(f"{month}\t{balance:.2f}\t{principal_paid:.2f}\t{interest:.2f}\t{monthly_payment:.2f}")
+        for i,(month,monthly_payment,principal_paid,interest,balance)in enumerate (repayment_schedule):
+            print(f"{month}\t{monthly_payment:.2f}\t{principal_paid:.2f}\t{interest:.2f}\t{balance:.2f}")
+           
             
-        columns=['Month','Principal Balance','Principal Payment','Interest Payment','Installment Amount']
+        columns=['Month','Installment Amount','Principal Payment','Interest Payment','Principal Balance']
         df=pd.DataFrame(repayment_schedule,columns=columns)
         df=df.set_index('Month')
         df['Principal Balance'] = df['Principal Balance'].astype('int64')
         df['Principal Payment'] = df['Principal Payment'].astype('int64')
         df['Interest Payment'] = df['Interest Payment'].astype('int64')
         df['Installment Amount'] = df['Installment Amount'].astype('int64')
-
-        result=st.table(df)
+ 
+        st.write("Total Interest Payment  : ",(f"{df['Interest Payment'].sum():.2f}"))
+        st.write("Monthly Installment Payment : ",round((monthly_payment),2))
+        
+ 
+        
+        
+        result=st.dataframe(df)
         st.success(result)
         
         
