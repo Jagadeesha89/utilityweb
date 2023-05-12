3ab031828967576ceda245b7c6b9ab2995e35acd
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-05-12 11:25:29 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -202,20 +202,19 @@ def main():
         if 'past' not in st.session_state:
             st.session_state['past'] = ['Hi!']
 
-        # Layout of input/response containers
+       # Layout of input/response containers
         input_container = st.container()
         colored_header(label='', description='', color_name='blue-30')
         response_container = st.container()
-        
-         # User input
+
+        # User input
         ## Function for taking user provided prompt as input
         def get_text():
-             input_text = st.text_input("You: ", "", key="input")
-             return input_text
-        
+            input_text = st.text_input("You: ", "", key="input")
+            return input_text
         ## Applying the user input box
         with input_container:
-             user_input = get_text()
+            user_input = get_text()
 
         # Response output
         ## Function for taking user prompt as input followed by producing AI generated responses
@@ -231,10 +230,10 @@ def main():
                 st.session_state.past.append(user_input)
                 st.session_state.generated.append(response)
         
-            if st.session_state['generated']:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-                    message(st.session_state["generated"][i], key=str(i))
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
         
             
     if page == "Select":
