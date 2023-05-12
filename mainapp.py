9ddb2247c741f061cc9819751258689b60061865
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-05-12 19:27:05 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -197,8 +197,9 @@ def main():
          ##About
          - This app is an LLM-powered chatbot built using,HugChat
          - This is AI powered chatbot, this may produce inacurate information about people, places, or facts
-        💡 Note: No API key required!
-        ''')
+         - Limited knowledge of world and events after 2021
+            💡 Note: No API key required!
+         ''')
         
         # Generate empty lists for generated and past.
         ## generated stores AI generated responses
