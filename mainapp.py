8ae58fc6c80ea7b6a6576363216e444fe3eb96e4
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-05-12 10:14:09 +0530

    Update mainapp.py
    App updated

--- mainapp.py
+++ mainapp.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
 
 
 ti=st.title("Welcome to Utility Services")
-page=st.selectbox("List of Services",("Select","EMI Calculator","Tax Calculator"))
+page=st.selectbox("List of Services",("Select","EMI Calculator","Tax Calculator","AI Powered Chatbot"))
 
 
 def main():
@@ -186,15 +190,57 @@ def main():
             st.subheader("Your Estimated Loan Repayment Schedule")
          
             st.dataframe(df)
-    
+            
+    def chatbot():
+        st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+        ## past stores User's questions
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ['Hi!']
+
+        # Layout of input/response containers
+        input_container = st.container()
+        colored_header(label='', description='', color_name='blue-30')
+        response_container = st.container()
+
+        # User input
+        ## Function for taking user provided prompt as input
+        def get_text():
+             input_text = st.text_input("You: ", "", key="input")
+             return input_text
+        ## Applying the user input box
+        with input_container:
+             user_input = get_text()
+
+        # Response output
+        ## Function for taking user prompt as input followed by producing AI generated responses
+        def generate_response(prompt):
+            chatbot = hugchat.ChatBot()
+            response = chatbot.chat(prompt)
+            return response
+
+        ## Conditional display of AI generated responses as a function of user provided prompts
+        with response_container:
+            if user_input:
+                response = generate_response(user_input)
+                st.session_state.past.append(user_input)
+                st.session_state.generated.append(response)
+        
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
             
             
     if page == "Select":
         st.write("Please select the services")
     elif page == "EMI Calculator":
         loan_repay()
-    else:
+    elif page == "Tax Calculator":
         tax_cal()
+    else:
+        
   
             
 if __name__=='__main__':
