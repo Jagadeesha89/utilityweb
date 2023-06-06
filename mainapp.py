88d2e1dffdda91476f9f2ed6b212a874d0791394
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-06 21:11:50 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -5,7 +5,7 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
-
+from hugchat.login import Login
 
 
 
