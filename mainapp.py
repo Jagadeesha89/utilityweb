68276b9c8f0419a0d09ba1a567ea2c2e663366c6
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-11 19:57:25 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -265,12 +265,12 @@ def main():
             if user_input:
                  response = generate_response(user_input)
                  st.session_state.past.append(user_input)
-                 response_placeholder = st.empty()
-                 response_placeholder.text("Generated Result: ")
-                 words = response.split()
-                 for word in words:
-                    response_placeholder.text("Generated Result: " + " ".join(words[:words.index(word) + 1]))
-                    time.sleep(0.1)
+             response_placeholder = st.empty()
+             response_placeholder.text("Generated Result: ")
+             words = response.split()
+             for word in words:
+                 response_placeholder.text("Generated Result: " + " ".join(words[:words.index(word) + 1]))
+                 time.sleep(0.1)
                  
                           
     if page == "Select":
