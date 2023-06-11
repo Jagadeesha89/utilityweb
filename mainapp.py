6e311ea87c7d2ef388be7a16b8955e8f5cfbf186
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-11 19:50:17 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -258,19 +258,20 @@ def main():
 
         ## Conditional display of AI generated responses as a function of user provided prompts
         with response_container:
-            with st.spinner('Generating response...'):
-                      time.sleep(5)
+            with st.spinner('Generating response please wait...'):
+                      time.sleep(7)
             st.success('Response generated')
+            words = response.split()
+            for word in words:
+                 response_placeholder.text("Generated Result: " + " ".join(words[:words.index(word) + 1]))
+                 time.sleep(0.1)
             if user_input:
                  response = generate_response(user_input)
                  st.session_state.past.append(user_input)
                  st.session_state.generated.append(response)
                  
                 
-        if st.session_state['generated']:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-                message(st.session_state["generated"][i], key=str(i))
+   
             
     if page == "Select":
         st.write("Please select the services")
