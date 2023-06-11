57d94bdeddf1f8bb8d1dacc01413500cc0e52e4a
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-11 19:05:09 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -228,9 +228,12 @@ def main():
                   placeholder="What would you like to say?",
                   label_visibility="collapsed",
              )
-                  b.form_submit_button("Send", use_container_width=True)
+        b.form_submit_button("Send", use_container_width=True)
             
-        
+        ## Applying the user input box
+        with input_container:
+            user_input = get_text()
+            
         # Log in to huggingface and grant authorization to huggingchat
         email="jaga.m.gowda@gmail.com"
         passwd="Jaga@9731"
