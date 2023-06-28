b967a2c2ba724b2ab404258947b08451b97f54bf
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-28 12:04:48 +0530

    Update smallchat.py

--- smallchat.py
+++ smallchat.py
@@ -2,8 +2,8 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-email="jaga.m.gowda@gmail.com"
-passwd="Jaga@9731"
+email = "jaga.m.gowda@gmail.com"
+passwd = "Jaga@9731"
 sign = Login(email, passwd)
 cookies = sign.login()
 # Save cookies to usercookies/<email>.json
@@ -22,20 +22,16 @@ if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-# Response output
-## Function for taking user prompt as input followed by producing AI generated response
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in hugchat.ChatBot(cookies=cookies.get_dict()),chatbot.chat(prompt),
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+    # Response output
+    # Function for taking user prompt as input followed by producing AI generated response
+    message_placeholder = st.empty()
+    full_response = ""
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    for response in chatbot.chat(prompt, messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages], stream=True):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
+
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+    with st.chat_message("assistant"):
+        message_placeholder.markdown(full_response)
