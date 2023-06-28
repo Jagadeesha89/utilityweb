59f5f8e4777526262433fb02c3198e6e5f052d11
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-28 14:40:37 +0530

    Update smallchat.py

--- smallchat.py
+++ smallchat.py
@@ -12,33 +12,33 @@ sign.saveCookies()
 
 st.title("ChatGPT-like clone")
 
-messages = []
-
-def type_writer_text(text):
-    st.write(text, end='', flush=True)
-
-def get_response(prompt):
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    full_response = ""
-    response = chatbot.chat(prompt, stream=True)
-    if isinstance(response, str):
-        full_response += response
-    else:
-        for choice in response.choices:
-            full_response += choice.delta.get("content", "")
-    return full_response
-
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("What is up?")
-if prompt:
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    full_response = get_response(prompt)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        type_writer_text(full_response)
+        message_placeholder = st.empty()
+        full_response = ""
+
+for response in openai.ChatCompletion.create(
+        model=st.session_state["openai_model"],
+        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+        stream=True,
+    ):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+st.session_state.messages.append({"role": "assistant", "content": full_response})
