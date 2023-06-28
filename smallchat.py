6b229dd9dd57bfb5370ab8efb12abe474229e781
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-28 10:19:46 +0530

    Update smallchat.py

--- smallchat.py
+++ smallchat.py
@@ -1,17 +1,27 @@
 import streamlit as st
-from streamlit_chat import message
 from hugchat import hugchat
 from hugchat.login import Login
-
 
 email="jaga.m.gowda@gmail.com"
 passwd="Jaga@9731"
 sign = Login(email, passwd)
 cookies = sign.login()
-
 # Save cookies to usercookies/<email>.json
 sign.saveCookies()
 
+st.title("ChatGPT-like clone")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 # Response output
 ## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
@@ -19,48 +29,17 @@ def generate_response(prompt):
     response = chatbot.chat(prompt)
     return response
 
-# Set the layout of the app
-st.layout = st.container()
-
-# Add a text input field
-text_input = st.text_input("Enter your message")
-
-# If the user enters a message, generate the result and display it
-if text_input:
-  # Get the user's query
-  query = text_input
-
-  # Generate the result
-  result = generate_response(query)
-
-  # Display the user's query, the result, and the conversation history
-  st.write("User query:", query)
-  st.write("Result:", result)
-  st.write("Conversation history:")
-  for message in result._conversation_history:
-    st.write(message)
-
-  # Once the user clicks on enter, remove the query from the prompt box
-  st.text_input("Enter your message", key=None)
-
-  # Display the generated response in typewriter effect
-  st.write(result, unsafe_allow_html=True, key="generated_response")
-  st.markdown("""
-  <style>
-  #generated_response {
-  animation: typewriting 2s infinite;
-  }
-
-  @keyframes typewriting {
-    0% {
-      opacity: 0;
-    }
-    25% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-    }
-  }
-  </style>
-  """, unsafe_allow_html=True)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in generate_response(prompt),
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
