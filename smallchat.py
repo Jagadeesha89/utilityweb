c7d53bb879ff61c1082f4c5110df8450af27bd43
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-06-28 13:00:37 +0530

    Update smallchat.py

--- smallchat.py
+++ smallchat.py
@@ -22,7 +22,6 @@ def get_response(prompt):
     else:
         for choice in response.choices:
             full_response += choice.delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
     return full_response
 
 if "messages" not in st.session_state:
