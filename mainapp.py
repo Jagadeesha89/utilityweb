12e676f1a2965ef431486843472dcf13ffaf73a9
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-07-01 17:35:39 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -128,7 +128,7 @@ def main():
            
     def loan_repay():
     
-        st.title("Loan EMI Calculator https://icons8.com/icon/vMkgItIdhG6L/calculator ")
+        st.title("Loan EMI Calculator 🖩 ")
         Loan=st.text_input("**Loan Amount**")
         Interest=st.text_input("**Enter Rate of Interest (without %)**")
         Tenor=st.text_input("**Number of months**")
