a5990c0eb28674bb5a0bb8e5d0c9a8b9302cdcda
Jagadeesha89 <119878086+Jagadeesha89@users.noreply.github.com>
2023-07-03 11:57:19 +0530

    Update mainapp.py
    Spinner update for generating the response

--- mainapp.py
+++ mainapp.py
@@ -222,11 +222,11 @@ def main():
             with st.chat_message("user"):
                 st.markdown(prompt)
             # Display assistant response in chat message container
-             
-                with st.chat_message("assistant"):
+             with st.spinner('Generating response....'):
+                 with st.chat_message("assistant"):
                     message_placeholder = st.empty()
                     full_response = ""
-                    with st.spinner('Generating response....'):
+                    
                         
                 def generate_response(prompt):
                     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
